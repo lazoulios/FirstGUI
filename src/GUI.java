82b8ab4cfeead8e1f51a6963e87e2f7293fc051b
Γεώργιος Λαζαρίδης <geolazkon@csd.auth.gr>
2023-12-01 22:59:41 +0200

    Changed the GUI. Made the login window smaller and setResizable(false) to avoid issues with component resizing.

--- src/GUI.java
+++ src/GUI.java
@@ -22,13 +22,19 @@ public class GUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Teamfight Tactics");
         frame.setIconImage(icon.getImage());
-        frame.setPreferredSize(new Dimension(900, 500));
+        frame.setPreferredSize(new Dimension(500, 300));
         frame.pack();
         frame.setLocation(300,200);
         frame.setVisible(true);
+        frame.setResizable(false);
         frame.setLayout(new BorderLayout());
-        panel.setLayout(new GridLayout(5,4,2,2));
+        panel.setLayout(null);
+        //usernameLabel operations
+        usernameLabel.setBounds(180,50,100,30);
+        //passwordLabel operations
+        passwordLabel.setBounds(180, 80, 100, 30);
         //usernameField operations
+        usernameField.setBounds(250,50,100,30);
         usernameField.addActionListener(e -> {
             String result = usernameField.getText();
             if (result.equals(info.get(passwordField.getText()))) {
@@ -38,7 +44,7 @@ public class GUI {
             }
         }); //Pressing enter tests the given username and password
         //passwordField operations
-        passwordField.setBounds(100, 100, 100, 30);
+        passwordField.setBounds(250, 80, 100, 30);
         passwordField.setSize(100, 30);
         passwordField.setEchoChar('*');
         passwordField.addActionListener(e->{
@@ -51,6 +57,7 @@ public class GUI {
             }
         }); //Pressing enter tests the given username and password
         //passwordButton operations
+        passwordButton.setBounds(200, 150, 100, 30);
         passwordButton.addActionListener(e -> {
             char[] password = passwordField.getPassword();
             String result = new String(password);
@@ -60,29 +67,25 @@ public class GUI {
                 JOptionPane.showMessageDialog(null, "Login Failed");
             }
         }); //Pressing the button tests the given username and password
-//        for (int i=0;i<12;i++) {
-//            frame.add(new JPanel());
+
+//        for(int i=0;i<5;i++){
+//            panel.add(new JPanel());
 //        }
-        for(int i=0;i<5;i++){
-            panel.add(new JPanel());
-        }
         panel.add(usernameLabel);
         panel.add(usernameField);
-        panel.add(new JPanel());
-        panel.add(new JPanel());
+//        panel.add(new JPanel());
+//        panel.add(new JPanel());
         panel.add(passwordLabel);
         panel.add(passwordField);
-        panel.add(new JPanel());
-        panel.add(new JPanel());
-        panel.add(new JPanel());
+//        panel.add(new JPanel());
+//        panel.add(new JPanel());
+//        panel.add(new JPanel());
         panel.add(passwordButton);
-        for(int i=0;i<4;i++){
-            panel.add(new JPanel());
-        }
-        frame.add(panel, BorderLayout.CENTER);
-//        for (int i=0;i<12;i++){
-//            frame.add(new JPanel());
+//        for(int i=0;i<4;i++){
+//            panel.add(new JPanel());
 //        }
+        frame.add(panel, BorderLayout.CENTER);
+
 
 
     }
